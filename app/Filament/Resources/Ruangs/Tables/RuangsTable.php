567289c4c5dc66916a9144f521f6c7c4f0b923a4
adensahwaludin <sekha1211@gmail.com>
2026-01-20 10:00:51 +0700

    Remove Jumlah Unit column and add Lihat Barang button in Ruang table

--- app/Filament/Resources/Ruangs/Tables/RuangsTable.php
+++ app/Filament/Resources/Ruangs/Tables/RuangsTable.php
@@ -2,10 +2,12 @@
 
 namespace App\Filament\Resources\Ruangs\Tables;
 
+use App\Filament\Resources\UnitBarangs\UnitBarangResource;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
+use Filament\Tables\Actions\Action;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -21,10 +23,6 @@ class RuangsTable
                 TextColumn::make('nama_ruang')
                     ->label('Nama Ruang')
                     ->searchable()
-                    ->sortable(),
-                TextColumn::make('unitBarangs_count')
-                    ->label('Jumlah Unit')
-                    ->counts('unitBarangs')
                     ->sortable(),
                 TextColumn::make('created_at')
                     ->label('Dibuat')
@@ -45,6 +43,12 @@ class RuangsTable
                 ViewAction::make(),
                 EditAction::make()
                     ->visible(fn () => auth()->user()->can('edit_ruangs')),
+                Action::make('lihat_barang')
+                    ->label('Lihat Barang')
+                    ->icon('heroicon-o-eye')
+                    ->color('info')
+                    ->url(fn ($record) => UnitBarangResource::getUrl('index', ['tableFilters[ruang_id][value]' => $record->id]))
+                    ->openUrlInNewTab(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
